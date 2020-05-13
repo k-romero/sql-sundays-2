f08992da123dd67e632066e2caf7a93ce9107499
k-romero <imkevinromero@gmail.com>
2020-05-12 22:33:17 -0400

    checkpoint

--- answers.sql
+++ answers.sql
@@ -72,3 +72,23 @@ SELECT team1, team2, player
 FROM game
     JOIN goal ON (id=matchid)
 WHERE player LIKE 'Mario%';
+
+SELECT player, teamid, coach, gtime
+FROM goal
+    JOIN eteam on teamid=id
+WHERE gtime<=10
+
+SELECT mdate, teamname
+FROM game
+    JOIN eteam ON (team1=eteam.id)
+WHERE coach = 'Fernando Santos';
+
+SELECT player
+FROM goal
+    JOIN game ON matchid=id
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT DISTINCT player
+FROM game
+    JOIN goal ON matchid = id
+WHERE (team1='GER' OR team2='GER') AND teamid <> 'GER';
