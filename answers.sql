25720b61fdbcea667811681377e953bc626150b4
k-romero <imkevinromero@gmail.com>
2020-05-12 23:09:18 -0400

    complete

--- answers.sql
+++ answers.sql
@@ -92,3 +92,43 @@ SELECT DISTINCT player
 FROM game
     JOIN goal ON matchid = id
 WHERE (team1='GER' OR team2='GER') AND teamid <> 'GER';
+
+SELECT teamname, COUNT(player)
+FROM eteam JOIN
+    goal ON id=teamid
+GROUP BY teamname
+ORDER BY teamname
+
+SELECT stadium, COUNT(player)
+FROM game
+    JOIN goal ON matchid = id
+GROUP BY stadium
+
+SELECT matchid, mdate, COUNT(mdate)
+FROM game
+    JOIN goal ON matchid = id
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY id
+
+SELECT matchid, mdate, COUNT(matchid)
+FROM game
+    JOIN goal ON matchid = id
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate;
+
+SELECT matchid, mdate, COUNT(player)
+FROM game
+    JOIN goal ON matchid = id
+WHERE (team1 = 'GER' OR team2 = 'GER')
+    AND teamid = 'GER'
+GROUP BY matchid, mdate
+
+SELECT mdate,
+    team1,
+    SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+    team2,
+    SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+FROM game JOIN goal ON matchid = id
+GROUP BY mdate,matchid,team1,team2
+ORDER BY mdate,matchid,team1,team2
+
